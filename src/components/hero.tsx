239c8ddc69e92568c644a53013727ec912b924d7
Krzema <krzema@example.com>
2023-05-12 13:00:29 +0200

    lepsza rensponsywność i animacja chmur

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,13 +1,12 @@
 export default function Hero() {
   return (
-    <h1 className="text-center text-3xl text-slate-900 m-36 p-3 bg-indigo-300 bg-opacity-25 shadow-2xl">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-      veritatis praesentium dolore labore voluptate nobis modi velit! Repellat,
-      deserunt repellendus.
-    </h1>
-    // <div className="relative top-0 left-0 items-center justify-center content-center text-center h-96">
-    //   {/* <img className="h-fit w-fit max-h-full" src={cloud1} alt="" /> */}
-    //   <Parallax className="bg-[url(./cloud1.png)] bg-cover" speed={-20} />
-    // </div>
+    <div className="m-12 md:m-12 flex justify-center">
+      <h1
+        className="p-3 md:p-10 text-center text-3xl max-w-lg shadow-2xl text-slate-900 
+    bg-indigo-300 bg-opacity-50"
+      >
+        Pomagam dzieciom rozwijać się i poznawać świat
+      </h1>
+    </div>
   );
 }
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -8,14 +8,14 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         bg-repeat-round h-screen w-full -z-20"
       ></div>
       <Parallax
-        className="bg-[url(./cloud1.png)] h-[40rem] w-full absolute top-0 bg-cover -z-10"
-        translateX={[10, -20]}
-        translateY={[-50, 40]}
+        className="bg-[url(./cloud1.png)] h-screen w-screen bg-center absolute bg-contain bg-no-repeat top-0 -z-10"
+        translateX={[50, -50]}
+        translateY={[-70, 40]}
       />
       <Parallax
-        className="bg-[url(./cloud2.png)] h-[40rem] w-full absolute top-0 bg-cover -z-10"
-        translateX={[-10, 20]}
-        translateY={[-30, 40]}
+        className="bg-[url(./cloud2.png)] h-screen w-screen bg-center absolute bg-contain bg-no-repeat top-0 -z-10"
+        translateX={[-50, 50]}
+        translateY={[-50, 40]}
         scale={[0.4, 1.3]}
       />
       <div className="container m-auto">{children}</div>
