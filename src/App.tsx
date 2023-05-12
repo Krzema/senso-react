11ab13b08448ca5a9d5901d0e2c3eb4882cbd78b
Krzema <krzema@example.com>
2023-05-12 12:14:23 +0200

    Revert "prettier + lint"
    This reverts commit 40fe1a1b62e1a789750d88debf4df42c5168bf15.

--- src/App.tsx
+++ src/App.tsx
@@ -10,65 +10,16 @@ function App() {
       <Header />
       <Hero />
       <IconSection />
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum
-        fugit praesentium, fugiat sunt dolore voluptate vel quae nam
-        exercitationem fuga, labore delectus. Facere ipsa voluptas numquam
-        voluptatibus dolorum ea tenetur!
-      </p>
-      <p>
-        Facere, maxime? Doloribus fugit omnis qui asperiores eius, tenetur
-        dolorum magnam ea quas, quod dignissimos consequatur? Explicabo,
-        cupiditate alias ad voluptas qui fugiat autem numquam placeat minima
-        aliquam illo natus.
-      </p>
-      <p>
-        Repudiandae, accusantium perferendis? Repellendus impedit nam tempore
-        molestiae sed saepe sequi. Fuga, autem quisquam. Corrupti, repudiandae
-        nulla voluptate odit commodi nemo earum consectetur! Dignissimos
-        explicabo, doloribus itaque hic ducimus vitae.
-      </p>
-      <p>
-        Odit reprehenderit fuga exercitationem error quo blanditiis iusto
-        perspiciatis sed mollitia omnis sapiente dolor ad, vel, at officia.
-        Tenetur temporibus odit, quibusdam ipsam id doloribus nobis rem
-        voluptatibus. Nisi, recusandae!
-      </p>
-      <p>
-        Tempore incidunt itaque sint ea temporibus enim dolores harum sequi
-        laudantium exercitationem. Enim aut, nam libero sed hic unde neque
-        maiores numquam consequatur dicta laboriosam illo saepe! Quod, mollitia
-        officiis!
-      </p>
-      <p>
-        Fugiat enim cum tempora blanditiis laborum harum, similique recusandae
-        dicta saepe quos possimus, perferendis eaque libero deserunt quibusdam
-        veritatis commodi temporibus adipisci. Excepturi vel repellendus magni,
-        quos tempora delectus ullam?
-      </p>
-      <p>
-        Placeat quos error tenetur assumenda quam delectus, a et necessitatibus
-        rerum tempora minus, accusantium eos impedit cum totam dolores. Corporis
-        debitis vero, at natus culpa illum quia voluptate ipsa delectus.
-      </p>
-      <p>
-        Nesciunt voluptatem consequuntur aperiam quisquam veritatis amet iusto
-        eligendi, laudantium, veniam id ut ad maxime placeat iure, eum
-        dignissimos obcaecati. Accusantium quae doloribus sequi autem officia
-        aperiam placeat aliquid deserunt!
-      </p>
-      <p>
-        Mollitia sunt assumenda eius facere dolor, iste nam quo ratione modi
-        temporibus facilis ducimus aliquam quaerat eligendi minus nihil dolorum
-        quas quos corrupti unde. Beatae placeat voluptatum quis obcaecati.
-        Deserunt.
-      </p>
-      <p>
-        Doloremque libero accusamus nesciunt error reiciendis officiis illo
-        dolorem? Nostrum ullam molestiae consequatur hic tenetur modi vitae,
-        necessitatibus blanditiis? Id nihil mollitia consequatur doloribus!
-        Eligendi fugit voluptatem adipisci quis provident.
-      </p>
+     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum fugit praesentium, fugiat sunt dolore voluptate vel quae nam exercitationem fuga, labore delectus. Facere ipsa voluptas numquam voluptatibus dolorum ea tenetur!</p>
+     <p>Facere, maxime? Doloribus fugit omnis qui asperiores eius, tenetur dolorum magnam ea quas, quod dignissimos consequatur? Explicabo, cupiditate alias ad voluptas qui fugiat autem numquam placeat minima aliquam illo natus.</p>
+     <p>Repudiandae, accusantium perferendis? Repellendus impedit nam tempore molestiae sed saepe sequi. Fuga, autem quisquam. Corrupti, repudiandae nulla voluptate odit commodi nemo earum consectetur! Dignissimos explicabo, doloribus itaque hic ducimus vitae.</p>
+     <p>Odit reprehenderit fuga exercitationem error quo blanditiis iusto perspiciatis sed mollitia omnis sapiente dolor ad, vel, at officia. Tenetur temporibus odit, quibusdam ipsam id doloribus nobis rem voluptatibus. Nisi, recusandae!</p>
+     <p>Tempore incidunt itaque sint ea temporibus enim dolores harum sequi laudantium exercitationem. Enim aut, nam libero sed hic unde neque maiores numquam consequatur dicta laboriosam illo saepe! Quod, mollitia officiis!</p>
+     <p>Fugiat enim cum tempora blanditiis laborum harum, similique recusandae dicta saepe quos possimus, perferendis eaque libero deserunt quibusdam veritatis commodi temporibus adipisci. Excepturi vel repellendus magni, quos tempora delectus ullam?</p>
+     <p>Placeat quos error tenetur assumenda quam delectus, a et necessitatibus rerum tempora minus, accusantium eos impedit cum totam dolores. Corporis debitis vero, at natus culpa illum quia voluptate ipsa delectus.</p>
+     <p>Nesciunt voluptatem consequuntur aperiam quisquam veritatis amet iusto eligendi, laudantium, veniam id ut ad maxime placeat iure, eum dignissimos obcaecati. Accusantium quae doloribus sequi autem officia aperiam placeat aliquid deserunt!</p>
+     <p>Mollitia sunt assumenda eius facere dolor, iste nam quo ratione modi temporibus facilis ducimus aliquam quaerat eligendi minus nihil dolorum quas quos corrupti unde. Beatae placeat voluptatum quis obcaecati. Deserunt.</p>
+     <p>Doloremque libero accusamus nesciunt error reiciendis officiis illo dolorem? Nostrum ullam molestiae consequatur hic tenetur modi vitae, necessitatibus blanditiis? Id nihil mollitia consequatur doloribus! Eligendi fugit voluptatem adipisci quis provident.</p>
     </Layout>
   );
 }
